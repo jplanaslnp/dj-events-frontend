a940e0e155ab83d57b23d7cffebc32ab485483fc
Jaume Planas <j.planaslnp@gmail.com>
2021-07-08 15:36:00 +0200

    Per page

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 export const NEXT_URL = process.env.NEXT_PUBLIC_NEXT_URL || 'http://localhost:3000'
 
-export const PER_PAGE = 2
+export const PER_PAGE = 4
+
+https://djeventsfrontend1231423.varcel.app
